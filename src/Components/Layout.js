af857cadad3b15ba324730fd13edc6dc7d8532bd
ismail bayoussef <b.ismail@ctd.ma>
2021-10-10 22:54:09 +0000

    x

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,10 +1,6 @@
 import React from "react";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import App from "../App";
 import About from "./About/About";
 import Navbar from "./Navbar/Navbar";
@@ -12,36 +8,43 @@ import Skills from "./Skills/Skills";
 import Projects from "./Projects/Projects";
 import Contact from "./Contact/Contact";
 export default function Layout() {
+  const timeout = { enter: 800, leave: 400 };
 
-    return (
-        <Router>
-            <div>
-
-
-                {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-                <Switch>
-                    <Route path="/about">
-                        <Navbar />
-                        <About />
-                    </Route>
-                    <Route path="/skills">
-                        <Skills />
-                    </Route>
-                    <Route path="/projects">
-                        <Navbar />
-                        <Projects />
-                    </Route>
-                    <Route path="/contact">
-                        <Navbar />
-                        <Contact />
-                    </Route>
-                    <Route path="/">
-                        <App />
-                    </Route>
-
-                </Switch>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <TransitionGroup component="div" className="App">
+        <CSSTransition
+          key={currentKey}
+          timeout={timeout}
+          classNames="pageSlider"
+          mountOnEnter={false}
+          unmountOnExit={true}
+        >
+          <div>
+            <Switch>
+              <Route path="/about">
+                <Navbar />
+                <About />
+              </Route>
+              <Route path="/skills">
+                <Navbar />
+                <Skills />
+              </Route>
+              <Route path="/projects">
+                <Navbar />
+                <Projects />
+              </Route>
+              <Route path="/contact">
+                <Navbar />
+                <Contact />
+              </Route>
+              <Route path="/">
+                <App />
+              </Route>
+            </Switch>
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
+    </Router>
+  );
 }
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,25 +1,23 @@
-import React, {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import BigContainer from "./BigContainer";
-import Spacer from "../../Utilities/Spacer";
 import picture from "../../media/pic.jpeg";
 import Description from "./Description";
 import Tablet from "./Tablet";
 import HeadingPrimary from "./HeadingPrimary";
-import Button from "../Home/Button";
 
 function Projects(children) {
-    const [data,setData] = useState([])
-    const url = "http://portfolio-backend1432.herokuapp.com/api/projects"
-    useEffect(()=> {fetch(url)
-        .then((response) => response.json())
-        .then(({data}) => setData(data))}   , [])
-  return data.map((t)=>(
-    <BigContainer close >
-
-        <div className="column">
-
-            <HeadingPrimary title={t.title} />
-        <Description  description={t.desc}/>
+  const [data, setData] = useState([]);
+  const url = "http://portfolio-backend1432.herokuapp.com/api/projects";
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then(({ data }) => setData(data));
+  }, []);
+  return data.map((t) => (
+    <BigContainer close>
+      <div className="column">
+        <HeadingPrimary title={t.title} />
+        <Description description={t.desc} />
       </div>
 
       <Tablet img={picture} />
